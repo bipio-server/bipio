51cbaebf65c75ff5a4700c68040a591c89a5af1a
Michael Pearson <github@x0rz.com>
2014-12-09 14:19:48 -0500

    cdn sync

--- src/modules/cdn/fs.js
+++ src/modules/cdn/fs.js
@@ -5,28 +5,29 @@ var fs = require('fs'),
   mime = require('mime');
 
 function FsProto(options) {
-  this.dataDir = options.data_dir;
+  if (options.data_dir) {
+    this.dataDir = options.data_dir;
 
-  this.tmpDir = options.data_dir + '/tmp'
-  this.permDir = options.data_dir + '/perm'
+    this.tmpDir = options.data_dir + '/tmp';
+    this.permDir = options.data_dir + '/perm';
+  }
 
-  this.cdnDir = options.cdn_dir;
+  if (options.cdn_dir) this.cdnDir = options.cdn_dir;
 };
 
 FsProto.prototype = {
 
   /**
-   * Saves a file to data_dir
-   *
-   * @param string fileName
-   * @param stream readStream
-   * @param object options
-   * @param function callback
-   */
+  * Saves a file from a string or readStream, and returns a fileStruct
+  *
+  * @param string dstFileName
+  * @param stream source readStream or string filepath to create source readStream
+  * @param function callback
+  */
   save: function() {
     var self = this;
-    if (arguments[0] && arguments[1] && typeof arguments[0] === 'string' && typeof arguments[arguments.length-1] === 'function' && self.parse_path(arguments[0], this.dataDir)) {
-      var dstFileName = path.resolve(self.permDir + '/' + arguments[0]),
+    if (arguments[0] && arguments[1] && typeof arguments[0] === 'string' && typeof arguments[arguments.length-1] === 'function' && self.parse_path(arguments[0], (self.dataDir ? self.dataDir : null))) {
+      var dstFileName = (self.permDir ? path.resolve(self.permDir + '/' + arguments[0]) : arguments[0]),
         next = arguments[arguments.length-1],
         readStream = ((typeof arguments[1] === 'string') ?  fs.createReadStream(arguments[1]) : arguments[1]);
 
@@ -35,17 +36,23 @@ FsProto.prototype = {
       writeStream.on('error', next);
 
       writeStream.on('finish', function(err) {
-        if (err) next(err);
+        if (err) next(err)
         self.normalize(dstFileName, next);
-      });
+        });
 
       readStream.pipe(writeStream);
     }
   },
 
+  /**
+  * Gets a file from a fileStruct and returns a new fileStruct and readStream
+  *
+  * @param object fileStruct
+  * @param function callback
+  */
   get: function() {
     var self = this;
-    if (arguments[0] && typeof arguments[0] === 'object' && typeof arguments[arguments.length-1] === 'function') {
+    if (arguments[0] && typeof arguments[0] ===  'object' && arguments[0].hasOwnProperty('localpath') && typeof arguments[arguments.length-1] === 'function') {
       var srcFile = arguments[0],
         next = arguments[arguments.length-1];
 
@@ -62,9 +69,9 @@ FsProto.prototype = {
     return multer({
       dest : this.tmpDir,
       onFileUploadComplete : function(file) {
-        file.localpath = file.path;
-        file.name = file.originalname;
-        file.type = file.mimetype;
+      file.localpath = file.path;
+      file.name = file.originalname;
+      file.type = file.mimetype;
       }
     });
   },
@@ -73,31 +80,31 @@ FsProto.prototype = {
     if (arguments[0] && typeof arguments[0] === 'string' && typeof arguments[arguments.length-1] === 'function') {
 
       var filePath = arguments[0],
-        next = arguments[arguments.length-1];
+      next = arguments[arguments.length-1];
 
       if (filePath.match(regex)) next("list() cannot be called on files, only directories.");
 
       fs.exists(filePath, function(exists) {
-          if (exists) {
-            fs.readdir(filePath, function (err, files) {
-              if (err) next(err);
+        if (exists) {
+        fs.readdir(filePath, function (err, files) {
+          if (err) next(err);
 
-              var results = [];
+          var results = [];
 
-              files.map(function (file) {
-                  return path.join(filePath, file);
-              }).filter(function (file) {
-                  return fs.statSync(file).isFile();
-              }).forEach(function (file) {
-                results.push(file.replace(filePath, ""));
-              });
-
-              next(null, results)
+          files.map(function (file) {
+            return path.join(filePath, file);
+          }).filter(function (file) {
+            return fs.statSync(file).isFile();
+          }).forEach(function (file) {
+          results.push(file.replace(filePath, ""));
           });
-          }
-          else {
-            next("Directory does not exist.");
-          }
+
+          next(null, results)
+        });
+        }
+        else {
+        next("Directory does not exist.");
+        }
       });
     }
   },
@@ -105,14 +112,14 @@ FsProto.prototype = {
   remove: function() {
     if (arguments[0] && typeof arguments[0] === 'string' && typeof arguments[arguments.length-1] === 'function') {
       var filePath = arguments[0],
-        next = arguments[arguments.length-1];
+      next = arguments[arguments.length-1];
       fs.exists(filePath, function(exists) {
-          if (exists) {
-            fs.unlink(filePath, next);
-          }
-          else {
-            next("File does not exist.");
-          }
+        if (exists) {
+        fs.unlink(filePath, next);
+        }
+        else {
+        next("File does not exist.");
+        }
       });
     }
   },
@@ -120,17 +127,17 @@ FsProto.prototype = {
   find: function() {
     if (arguments[0] && typeof arguments[0] === 'string' && typeof arguments[arguments.length-1] === 'function') {
       var filePath = arguments[0],
-        next = arguments[arguments.length-1];
+      next = arguments[arguments.length-1];
       fs.exists(filePath, function(exists) {
-          if (exists) {
-            fs.stat(filePath, function(err, file) {
-            if (err) next(err);
-            next(null, file);
-          });
-          }
-          else {
-            next("File does not exist.");
-          }
+        if (exists) {
+        fs.stat(filePath, function(err, file) {
+        if (err) next(err);
+        next(null, file);
+        });
+        }
+        else {
+        next("File does not exist.");
+        }
       });
     }
   },
@@ -165,23 +172,13 @@ FsProto.prototype = {
     fs.stat(filePath, function(err, stats) {
       if (err) next(err);
       next(null, {
-            size : stats.size,
-            localpath : filePath,
-            name : self.get_filename_from_path(filePath),
-            type : mime.lookup(filePath),
-            encoding : 'binary'
-          });
-    });
-  },
-
-  normalize_sync: function(filePath) {
-    return {
-        size : fs.statSync(filePath).size,
+        size : stats.size,
         localpath : filePath,
         name : self.get_filename_from_path(filePath),
         type : mime.lookup(filePath),
         encoding : 'binary'
-      }
+        });
+    });
   }
 
 };
