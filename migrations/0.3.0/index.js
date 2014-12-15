42c116ac58207d483e26dfb4e5f8d4b4f4770773
Michael Pearson <github@x0rz.com>
2014-12-15 14:46:58 -0500

    added 0.3.0 migration stub

---
+++ migrations/0.3.0/index.js
@@ -0,0 +1,69 @@
+var fs = require('fs'),
+  Migration = {
+    run : function(app, configPath, next) {
+
+      // pseudo
+
+      /*
+
+      rename config : "cdn_public": "http://localhost/static/img/cdn",
+      to : "cdn_public": "http://localhost/static",
+
+
+      add config :
+
+      "cdn" : {
+        "strategy" : "fs",
+        "config" : {
+          "data_dir" : "derived from datadir"
+        }
+      }
+
+
+      remove configs :
+
+      cdn
+      datadir
+
+      */
+
+
+
+      next();
+      return;
+
+
+      var config = JSON.parse(fs.readFileSync(configPath)),
+        rootPath = GLOBAL.SERVER_ROOT + '/../',
+        delta = false;
+
+      if (!config.modules) {
+        config.modules = {};
+        delta = true;
+      }
+
+      // example
+      if (!config.modules.auth && config.auth) {
+        config.modules.auth = {};
+        config.modules.auth.strategy = config.auth.type;
+        config.modules.auth.config = app._.clone(config.auth.config);
+        delete config.auth;
+        delta = true;
+      }
+
+      if (delta) {
+        fs.writeFile(configPath , JSON.stringify(config, null, 2), function(err) {
+          if (err) {
+            next(err, 'error');
+          } else {
+            console.info("\nConfig written to : " + configPath + "\n");
+            next();
+          }
+        });
+      } else {
+        next('Nothing To Do');
+      }
+    }
+  }
+
+module.exports = Migration;
