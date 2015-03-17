6fd7147fbac2c7d8decfc48d81765645acf830c4
Michael Pearson <github@x0rz.com>
2015-03-17 14:25:45 -0400

    restored bad merge

--- src/models/bip.js
+++ src/models/bip.js
@@ -55,6 +55,7 @@ function generate_random_base() {
  */
 function endLifeParse(end_life) {
   var seconds, d;
+
   // passed validation but isn't a number, then set it zero (never end based on impressions)
   if (isNaN(parseInt(end_life.imp))) {
     end_life.imp = 0;
@@ -62,6 +63,14 @@ function endLifeParse(end_life) {
 
   if (!end_life.time) {
     end_life.time = 0;
+  } else if (end_life.time !== '0' && end_life.time !== 0 && end_life.time !== '') {
+    try {
+      d = new Date(Date.parse(end_life.time));
+      if (d.getTime() != 0) {
+        end_life.time = Math.floor(d.getTime() / 1000);
+      }
+    } catch (e) {
+    }
   }
 
   return end_life;
