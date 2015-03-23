a6f5b439523248bcafc31d205f64272f0b4316d7
Alfonso Gober <alfonso.gober.jr@gmail.com>
2015-03-23 19:46:54 +0000

    BIP-310 OEmbed: switched to iframe and config var for oembed widget

--- src/router.js
+++ src/router.js
@@ -562,37 +562,39 @@ module.exports = {
   */
 
   express.get('/oembed/*', function(req, res) {
-    if (req.query.url) {
+    if (req.query.url && GLOBAL.CFG.oembed_host) {
       
       var shareId = req.query.url.split('/')[req.query.url.split('/').length - 1]
       
       dao.find('bip_share', { id : shareId }, function(err, result) {
         
         if (err) {
-          res.status(500).end(err)
+          res.status(500).json(err)
         }
 
         res.json({
           provider_name: "Bipio",
-          provider_url: "http://"+req.headers.host,
+          provider_url: GLOBAL.CFG.website_public,
           version: "1.0",
-          html: "<div class=\"bip-select-info\">"+
-                  "<div class=\"bip-preview middle pull-left\">"+
-                    "<img alt=\""+ result.note +"\" title=\""+ result.name +"\" class=\"tooltipped hub-icon hub-icon-32 source-icon\" data-placement=\"top\" data-container=\"body\" src=\"CENTER_IMAGE_URL\" />"+
-                    "<ul>"+
-                      "<li><img class=\"mini tooltipped hub-icon\" data-container=\"body\" data-placement=\"top\" title=\"ACTION_TITLE\" src=\"POD_IMAGE_URL\" /></li>"+
-                    "</ul>"+
-                  "</div>"+
-                  "<a href=\"http://"+ req.headers.host +"/signup?p=%2Fdash%23community%2F"+ result.id +"\"></a>"+
-                  "<div>"+
-                    "<strong class=\"name\">"+ result.name +"</strong>"+
-                  "</div>"+
-                  "<div class=\"pull-left\">"+
-                    "<small class=\"description\">"+ result.note +"</small>"+
-                  "</div><button data-target=\"http://"+ req.headers.host +"/signup?p=%2Fdash%23community%2F"+ result.id +"\" class=\"btn pull-right\">"+
-                  "<h2>+</h2></button>"+
-                  "<div class=\"clearfix\"></div>"+
-                "</div>"
+          html: "<iframe src=\""+ GLOBAL.CFG.oembed_host + "/" + result.id +"\">"+
+                  /*"<div class=\"bip-select-info\">"+
+                    "<div class=\"bip-preview middle pull-left\">"+
+                      "<img alt=\""+ result.note +"\" title=\""+ result.name +"\" class=\"tooltipped hub-icon hub-icon-32 source-icon\" data-placement=\"top\" data-container=\"body\" src=\"CENTER_IMAGE_URL\" />"+
+                      "<ul>"+
+                        "<li><img class=\"mini tooltipped hub-icon\" data-container=\"body\" data-placement=\"top\" title=\"ACTION_TITLE\" src=\"POD_IMAGE_URL\" /></li>"+
+                      "</ul>"+
+                    "</div>"+
+                    "<a href=\"http://"+ req.headers.host +"/signup?p=%2Fdash%23community%2F"+ result.id +"\"></a>"+
+                    "<div>"+
+                      "<strong class=\"name\">"+ result.name +"</strong>"+
+                    "</div>"+
+                    "<div class=\"pull-left\">"+
+                      "<small class=\"description\">"+ result.note +"</small>"+
+                    "</div><button data-target=\"http://"+ req.headers.host +"/signup?p=%2Fdash%23community%2F"+ result.id +"\" class=\"btn pull-right\">"+
+                    "<h2>+</h2></button>"+
+                    "<div class=\"clearfix\"></div>"+
+                  "</div>"+*/
+                "</iframe>"
         });
       });
 
