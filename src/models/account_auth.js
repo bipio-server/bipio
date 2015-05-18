647834ad9f956e1b607b48f7f376c8ee5b5df352
Michael Pearson <github@x0rz.com>
2015-05-18 16:39:23 -0400

    cryptSave in account_auth shouldn't attempt to crypt undefined

--- src/models/account_auth.js
+++ src/models/account_auth.js
@@ -107,10 +107,14 @@ function _encStr(s, toUnicode) {
 }
 
 function cryptSaveObj(value) {
-  //var strVal = (new Buffer(JSON.stringify(value), 'utf-8' )).toString('ascii')
-  //return cryptSave(JSON.stringify(strVal));
-  var strVal = _encStr(value, false)
-  return cryptSave(JSON.stringify(strVal));
+  if (value) {
+    //var strVal = (new Buffer(JSON.stringify(value), 'utf-8' )).toString('ascii')
+    //return cryptSave(JSON.stringify(strVal));
+    var strVal = _encStr(value, false)
+    return cryptSave(JSON.stringify(strVal));
+  } else {
+    return value;
+  }
 }
 
 AccountAuth.id = '';
