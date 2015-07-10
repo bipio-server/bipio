0c51a168b14c528ee59cbd6f131f9637f63d33d1
Michael Pearson <github@x0rz.com>
2015-07-10 19:05:28 -0400

    testing for avatar before assuming string

--- src/models/account_option.js
+++ src/models/account_option.js
@@ -261,7 +261,7 @@ AccountOption.entityValidators = {
 
 AccountOption.preSave = function(accountInfo, next) {
     var self = this;
-			if (0 === this.avatar.toLowerCase().indexOf('http') && this.avatar && 0 !== this.avatar.indexOf(CFG.cdn_public ) ) {
+			if (this.avatar && 0 === this.avatar.toLowerCase().indexOf('http') && 0 !== this.avatar.indexOf(CFG.cdn_public ) ) {
 			app.modules.cdn.saveAvatar(this.owner_id, request.get(this.avatar), '/cdn/img/av/', function(err, avatarPath) {
 				if (err) {
 					next(err);
